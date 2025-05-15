591a33d060d7a6a70671046768a27990ef93e7c9
Manse <pedro.manse@dmk3.com.br>
2025-05-15 13:00:34 -0300

    tiny: removed comments; can't use newer version because of Cargo.lock v4

--- tiny/default.nix
+++ tiny/default.nix
@@ -1,4 +1,4 @@
-{ rustPlatform, fetchFromGitHub, pkgs }:
+{ rustPlatform, fetchFromGitHub }:
 rustPlatform.buildRustPackage rec {
   pname = "tinyirc";
   version = "0.13.0";
@@ -10,17 +10,5 @@ rustPlatform.buildRustPackage rec {
     sha256 = "/o+zin6/fuzmRV3H8FAfbnLQLFgTJyCGPT4HloMT6/8=";
   };
 
-  #useFetchCargoVendor = true;
   cargoSha256 = "zQeV9WAtX0fSMQ/GkyEnUg/bEI7lRJGp2fT1QL2bYKM=";
-
-  #buildPhase = "
-  #  runHook preBuild
-  #  cargo build
-  #  runHook postBuild
-  #";
-
-  #cargoLock = {
-  #  lockFile = ./Cargo.lock;
-  #};
-
 }
