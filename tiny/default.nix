29bfb984b06488884536903d09b212a6ed07467c
Manse <pedro.manse@dmk3.com.br>
2025-05-15 13:24:29 -0300

    tiny: downgrade Cargo.lock version, upgrade program version

--- tiny/default.nix
+++ tiny/default.nix
@@ -5,10 +5,14 @@ rustPlatform.buildRustPackage rec {
 
   src = fetchFromGitHub {
     owner = "osa1";
-    repo = "tiny";
-    rev = "eaed50d";
-    sha256 = "/o+zin6/fuzmRV3H8FAfbnLQLFgTJyCGPT4HloMT6/8=";
+    repo = "v0.13.0";
+    rev = "73f2401";
+    sha256 = "lbG07IlBhvY1vFP0aU6mInbiVC60Y9RRAj15tCPlUVM=";
   };
 
-  cargoSha256 = "zQeV9WAtX0fSMQ/GkyEnUg/bEI7lRJGp2fT1QL2bYKM=";
+  cargoPatches = [
+    ./downgrade-Cargo.lock-version.patch # Cargo.lock version 4 -> 3
+	];
+
+  cargoHash = "sha256-YDDkYBsg/ORFLgvYf6A/xzgFPAaL5PfzCutLop8v+R8=";
 }
