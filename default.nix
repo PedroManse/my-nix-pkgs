82f3bf012353fcc6fc7d6f2474b3dc66c72c311c
Manse <122305437+PedroManse@users.noreply.github.com>
2025-12-05 20:17:44 -0300

    removed tiny

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { };
+  pkgs = import <nixpkgs> { };
 in {
   hello = pkgs.callPackage ./hello { };
-  tiny = pkgs.callPackage ./tiny { };
 }
